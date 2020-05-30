5acd086c0e28b0b1cb0f23e17db75bf76ffd49cd
Mike <inboxema2015@gmail.com>
2020-05-30 21:38:36 +0300

    finishes first iteration of functional test

--- jmad/tests.py
+++ jmad/tests.py
@@ -77,10 +77,18 @@ class StudentsCaseTest(LiveServerTestCase):
         self.assertEqual(len(second_search_results), 2)
 
         # He clicks on a search result.
+        second_search_results[0].click()
 
         # The solo page has the title, artist and album for
         # this particular solo.
-        self.fail('Incomplete Test')
+        self.assertEqual(self.browser.current_url, '{}/solos/2/'.format(self.live_server_url))
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-artist').text, 'Cannonball Adderley')
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-track').text, 'All Blues')
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-album').text,'Kind of Blue')
+        # He also sees the start time and end time of the solo.
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-start-time').text, '2:06')
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-end-time').text, '4:01')
+
 
 
 
