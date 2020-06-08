648dc07c3376609a69eddb1d0ca9d8b386fadba9
Mike <inboxema2015@gmail.com>
2020-06-08 14:45:01 +0300

    Adds str, ordering, and columns for Track admin

--- albums/admin.py
+++ albums/admin.py
@@ -4,5 +4,10 @@ from django.contrib import admin
 from albums.models import Album, Track
 
 
+class TrackAdmin(admin.ModelAdmin):
+    model = Track
+    list_display = ['album', 'name', 'track_number']
+
+
 admin.site.register(Album)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
