463b5ca1c71a02dd7e3feffbe90a7b1c11a2d131
Mike <inboxema2015@gmail.com>
2020-06-08 08:43:59 +0300

    Extends functional test for model and URL refactor

--- jmad/tests.py
+++ jmad/tests.py
@@ -90,15 +90,27 @@ class StudentsCaseTest(LiveServerTestCase):
         # The solo page has the title, artist and album for
         # this particular solo.
 #         import pdb; pdb.set_trace()
-        self.assertEqual(self.browser.current_url, '{}/solos/2/'.format(self.live_server_url))
+#         self.assertEqual(self.browser.current_url, '{}/solos/2/'.format(self.live_server_url))
+#         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-artist').text, 'Cannonball Adderley')
+#         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-track').text, 'All Blues')
+#         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-album').text, 'Kind of Blue')
+#         # He also sees the start time and end time of the solo.
+#         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-start-time').text, '2:06')
+#         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-end-time').text, '4:01')
+
+        # on the solo page...
+        self.assertEqual(
+            self.browser.current_url,
+            self.live_server_url + '/recordings/kind-of-blue/all-blues/cannonball-adderley'
+        )
+
+        # he sees the artist...
         self.assertEqual(self.browser.find_element_by_css_selector('#jmad-artist').text, 'Cannonball Adderley')
-        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-track').text, 'All Blues')
-        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-album').text, 'Kind of Blue')
-        # He also sees the start time and end time of the solo.
-        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-start-time').text, '2:06')
-        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-end-time').text, '4:01')
 
+        # the track title (with a count of solos)...
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-track').text, 'All Blues [2 solos]')
 
-
+        # and the album title (with track count) for this solo.
+        self.assertEqual(self.browser.find_element_by_css_selector('#jmad-album'). text, 'Kind of Blue [3 tracks]')
 
 
